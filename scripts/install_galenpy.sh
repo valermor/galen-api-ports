6b4d68299e4418a8c031be055fea3916abadd284
Valerio Morsella <valerio.morsella@skyscanner.net>
2015-05-03 18:55:16 +0200

    Anticipated fix for issue in running acceptance tests.

--- scripts/install_galenpy.sh
+++ scripts/install_galenpy.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 chmod -R +x galen-api-ports
-cd galen-api-ports
 echo "BUILDING GALENPY"
 ./distribute_galenpy.sh -no-upload
 export GALENPY_VERSION=$(python -c 'import py; print py.__version__')
 echo "INSTALLING GALENPY"
 pip install dist/galenpy-${GALENPY_VERSION}-py2.py3-none-any.whl
 pip freeze
-cd ..
